74e454ee37243533a9ca1734da2650ffc5856342
harumhl <haru.mhl@gmail.com>
2017-06-01 21:54:06 -0400

    added step 6 of KE-train & changed indention

--- trofi.py
+++ trofi.py
@@ -51,52 +51,65 @@ i = 0
 while True:
     
 #5: s-simL i+1(sx, sy) := P wx∈sx p(wx, sx)maxwy∈sy w-simi(wx, wy), for all sx, sy ∈ S × L
-for s_x in S:
-    for s_y in L:
-        
-        summed_val = 0
-        for w_x in s_x:
+    for s_x in S:
+        for s_y in L:
             
-            # max WSM [w_x, w_y]
-            max_of_WSM = 0
-            for w_y in s_y:
-                if WSM [w_x, w_y] > max_of_WSM:
-                    max_of_WSM = WSM [w_x, w_y]
-                    
+            summed_val = 0
+            for w_x in s_x.split():
+                
+                # max WSM [w_x, w_y]
+                max_of_WSM = -1
+                for w_y in s_y.split():
+                    if WSM [w_x, w_y] > max_of_WSM:
+                        max_of_WSM = WSM [w_x, w_y]
+        
             summed_val += p(w_x, s_x) * max_of_WSM
         SSM_L[s_x, s_y] = summed_val
 
 # 6: s-simN i+1(sx, sy) := P wx∈sx p(wx, sx)maxwy∈sy w-simi(wx, wy), for all sx, sy ∈ S × N
-# everything is same as step 5 except that we use N instead of L & SSM_N instead of SSM_L
+    for s_x in S:
+        for s_y in N:
+        
+            summed_val = 0
+                for w_x in s_x.split():
+                
+                    # max WSM [w_x, w_y]
+                    max_of_WSM = -1
+                    for w_y in s_y.split():
+                        if WSM [w_x, w_y] > max_of_WSM:
+                            max_of_WSM = WSM [w_x, w_y]
+        
+                summed_val += p(w_x, s_x) * max_of_WSM
+            SSM_N[s_x, s_y] = summed_val
 
 #7: for wx, wy ∈ W × W do 
-for w_x in W:
-    for w_y in W:
+    for w_x in W:
+        for w_y in W:
         
 #8: w-simi+1(wx, wy) := (i = 0 P sx3wx p(wx, sx)maxsy3wy s-simI i (sx, sy)
 #                        else P sx3wx p(wx, sx)maxsy3wy {s-simL i (sx, sy),s-simN i (sx, sy)}
-        for s_x in sentences_containing (w_x, ???):
+            for s_x in sentences_containing (w_x, ???):
         
-            summed_val = 0
-            for s_y in sentences_containing (w_y, ???):
-                max_of_SSM = 0
-                if i == 0:
-                    if SSM [s_x, s_y] > max_of_SSM:
-                        max_of_SSM = SSM [s_x, s_y]
-                else:
-                    if SSM_L [s_x, s_y] > max_of_SSM:
-                        max_of_SSM = SSM_L [s_x, s_y]
-                    if SSM_N [s_x, s_y] > max_of_SSM:
-                        max_of_SSM = SSM_N [s_x, s_y]
-            summed_val += p(w_x, s_x) * max_of_SSM
-        WSM [w_x, w_y] = summed_val
+                summed_val = 0
+                for s_y in sentences_containing (w_y, ???):
+                    max_of_SSM = 0
+                    if i == 0:
+                        if SSM [s_x, s_y] > max_of_SSM:
+                            max_of_SSM = SSM [s_x, s_y]
+                    else:
+                        if SSM_L [s_x, s_y] > max_of_SSM:
+                            max_of_SSM = SSM_L [s_x, s_y]
+                        if SSM_N [s_x, s_y] > max_of_SSM:
+                            max_of_SSM = SSM_N [s_x, s_y]
+                summed_val += p(w_x, s_x) * max_of_SSM
+            WSM [w_x, w_y] = summed_val
         
 #9: end for
 #10: if ∀wx, maxwy {w-simi+1(wx, wy) − w-simi(wx, wy)} ≤  then
 # when to end. I'll just set it based on i value
 
 #11: break # algorithm converges in 1steps.
-        break
+    #break
 
 #12: end if
 #13: i := i + 1
